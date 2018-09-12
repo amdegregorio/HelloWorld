f831b95a70aa79412bb51d0fbef0f32eb5c85243
amdegregorio <test@test.com>
2018-09-12 06:44:23 -0400

    Added some java doc

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
 
 public class HelloWorld {
 
+	/**
+	 * Main entry point for Hello World.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		//Basic system out command
 		System.out.println("Hello World!");
