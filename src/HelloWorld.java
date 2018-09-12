c39a44f472334f59e98dc77e4be6120e8c7050dc
amdegregorio <test@test.com>
2018-09-12 06:40:49 -0400

    Added a comment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		//Basic system out command
 		System.out.println("Hello World!");
 		System.out.println("Hello, Universe!");
+		
 	}
 
 }
